59e913c90978cd16996673a2b4053ff50b479fa3
dessert28 <101861525+dessert28@users.noreply.github.com>
2025-03-03 09:53:09 +0800

    Add files via upload

---
+++ library_system/library_system.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+int menu()
+{
+	int x;
+	printf("\n");
+	printf("---------------------\n");
+	printf("|      test         |\n");
+	printf("|                   |\n");
+	printf("|1.管理员模式.      |\n");
+	printf("|2.客户模式.        |\n");
+	printf("|3.退出.            |\n");
+	printf("请选择：...\n");
+	printf("---------------------\n");
+	scanf("%d,&x");
+	return x;
+}
+int main()
+{
+	
+	int a;
+	do
+	{
+		system("cls");
+		
+	}
+}
